c10bd2f3f4adae736d86327586e068f693c822e3
dzanin <davide.zanin@supsi.ch>
2021-03-05 00:44:28 +0100

    Adding Vet Page and controller. closes #7.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dz on 05/03/21.
+ */
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets","/vets/index", "/vets/index.html"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+}
